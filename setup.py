94c6f6003d521102cbf72a796da8e691c7cf42a1
Mateusz Leputa <56775662+MatInGit@users.noreply.github.com>
2020-10-24 19:17:55 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,10 +13,11 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/MatInGit/Rulern",
     packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
+    install_requires=[
+       "numpy",
+       "pandas",
+       "timeit",
+       "time"
+   ],
     python_requires='>=3.7',
 )
